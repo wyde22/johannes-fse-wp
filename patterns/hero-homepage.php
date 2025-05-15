ee7cf99dc5edd7e542364caab0a4057872dde0d6
dewy <dewymercerais@gmail.com>
2025-05-15 18:49:40 +0200

    build: :lipstick: add hero homepage pattern

---
+++ patterns/hero-homepage.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+   * Title: Hero homepage
+   * Slug: johannesfse-hero-homepage
+   * Description: Hero homepage with left content and buttons
+   * Categories: banner
+   * Keywords: hero, homepage
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"categories":["banner"],"patternName":"core/block/2799744","name":"Hero homepage"},"className":"pattern-johannes","style":{"spacing":{"padding":{"top":"var:preset|spacing|3xlarge","bottom":"var:preset|spacing|3xlarge","left":"0","right":"0"}}},"backgroundColor":"johannes-grays-50","layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group pattern-johannes has-johannes-grays-50-background-color has-background" style="padding-top:var(--wp--preset--spacing--3-xlarge);padding-right:0;padding-bottom:var(--wp--preset--spacing--3-xlarge);padding-left:0"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group alignwide" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"p-small"} -->
+<p class="has-p-small-font-size" style="text-transform:uppercase">A place for the over-title</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"typography":{"textTransform":"none"}},"fontSize":"xxl"} -->
+<h2 class="wp-block-heading has-xxl-font-size" style="text-transform:none">This is the place for a title</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|large"}}}} -->
+<p style="padding-top:0;padding-bottom:var(--wp--preset--spacing--large)"><br>This is a place for a exerpt. Lorem ipsum dolor sit amet consectetur. At fusce ac netus non nam ut amet. Arcu tempor rhoncus varius purus aliquam nunc. Interdum auctor commodo sed suspendisse eget velit mauris a porttitor. Placerat convallis massa ante tellus viverra mauris ultricies dignissim feugiat. Nibh nisl est tempus aliquam massa tempus bibendum.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":"0px"},"typography":{"textTransform":"uppercase"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline","style":{"border":{"radius":"0px"},"typography":{"textTransform":"uppercase"}}} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
