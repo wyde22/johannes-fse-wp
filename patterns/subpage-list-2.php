da7e4953ab38e55e4dea4ad5bb8fc9a6f5e8619d
dewy <dewymercerais@gmail.com>
2025-05-21 16:18:48 +0200

    build: :lipstick: add another subpage list 2 pattern and the css rules for responsive

---
+++ patterns/subpage-list-2.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+   * Title: Subpage list 2
+   * Slug: johannesfse-subpage-list-2
+   * Description: Subpage list with intro and quick navigation to content
+   * Categories: intro
+   * Keywords: intro
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Subpage list 2"},"className":"subpage-list-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group subpage-list-2"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"customOverlayColor":"#FFF","isUserOverlayColor":true,"isDark":false,"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignwide is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#FFF"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"35rem"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"left","level":1,"fontSize":"large"} -->
+<h1 class="wp-block-heading has-text-align-left has-large-font-size">This is the place for a hub title</h1>
+<!-- /wp:heading -->
+
+<!-- wp:post-excerpt {"style":{"spacing":{"padding":{"top":"var:preset|spacing|2xlarge","bottom":"var:preset|spacing|2xlarge"}}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|base","bottom":"var:preset|spacing|base","left":"var:preset|spacing|base","right":"var:preset|spacing|base"}}},"backgroundColor":"johannes-grays-0","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group has-johannes-grays-0-background-color has-background" style="padding-top:var(--wp--preset--spacing--base);padding-right:var(--wp--preset--spacing--base);padding-bottom:var(--wp--preset--spacing--base);padding-left:var(--wp--preset--spacing--base)"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"p-default"} -->
+<p class="has-p-default-font-size" style="text-transform:uppercase">direct access</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list {"className":"is-style-list-with-arrow","style":{"spacing":{"padding":{"top":"var:preset|spacing|xlarge"}}}} -->
+<ul style="padding-top:var(--wp--preset--spacing--xlarge)" class="wp-block-list is-style-list-with-arrow"><!-- wp:list-item -->
+<li><strong>Non adipiscing ultricies et</strong></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><strong>Non adipiscing ultricies et</strong></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><strong>Non adipiscing ultricies et</strong></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><strong>Non adipiscing ultricies et</strong></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
