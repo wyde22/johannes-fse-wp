c595f276a4e544c0a663b3578395d98d52159a6d
dewy <dewymercerais@gmail.com>
2025-05-21 17:08:31 +0200

    build: :lipstick: add image large 1 pattern with css rules for responsive

---
+++ patterns/image-large-1.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+   * Title: Image large 1
+   * Slug: johannesfse-image-large-1
+   * Description: Image large with title and buttons
+   * Categories: image
+   * Keywords: image, large
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Image large 1"},"className":"image-large-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-group image-large-1"><!-- wp:post-featured-image {"height":"25rem","align":"wide"} /-->
+
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|2xlarge","left":"var:preset|spacing|2xlarge","top":"var:preset|spacing|xlarge","bottom":"var:preset|spacing|xlarge"}}},"backgroundColor":"johannes-grays-0","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-johannes-grays-0-background-color has-background" style="padding-top:var(--wp--preset--spacing--xlarge);padding-right:var(--wp--preset--spacing--2-xlarge);padding-bottom:var(--wp--preset--spacing--xlarge);padding-left:var(--wp--preset--spacing--2-xlarge)"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"p-small"} -->
+<p class="has-p-small-font-size" style="text-transform:uppercase">A place for the over title</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading">This is the place for a page title</h1>
+<!-- /wp:heading -->
+
+<!-- wp:post-excerpt {"style":{"spacing":{"padding":{"top":"var:preset|spacing|large","bottom":"var:preset|spacing|large"}}}} /-->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":"0px"},"typography":{"textTransform":"uppercase"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline","style":{"border":{"radius":"0px"},"typography":{"textTransform":"uppercase"}}} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
