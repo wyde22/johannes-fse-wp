046680dae620a8e2af6f96561a5d5b605e97a2aa
dewy <dewymercerais@gmail.com>
2025-05-21 12:14:48 +0200

    build: :lipstick: add subpage list 1 pattern with variation css rules for responsive

---
+++ patterns/subpage-list-1.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+   * Title: Subpage list 1
+   * Slug: johannesfse-subpage-list-1
+   * Description: Subpage list with intro and quick navigation to content
+   * Categories: intro
+   * Keywords: intro
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Subpage list 1"},"className":"subpage-list-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-group subpage-list-1"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|xlarge","bottom":"var:preset|spacing|2xlarge"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--xlarge);padding-bottom:var(--wp--preset--spacing--2-xlarge)"><!-- wp:group {"style":{"layout":{"selfStretch":"fixed","flexSize":"35rem"}},"layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading">This is the place for a hub title</h2>
+<!-- /wp:heading -->
+
+<!-- wp:post-excerpt {"style":{"spacing":{"padding":{"top":"var:preset|spacing|xlarge"}}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:list {"className":"is-style-list-with-arrow"} -->
+<ul class="wp-block-list is-style-list-with-arrow"><!-- wp:list-item -->
+<li>Non adipiscing ultricies etn adipiscing ultricies et</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Non adipiscing ultricies etn adipiscing ultricies et</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Non adipiscing ultricies etn adipiscing ultricies et</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Non adipiscing ultricies etn adipiscing ultricies et</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:group -->
+
+<!-- wp:post-featured-image /--></div>
+<!-- /wp:group -->
