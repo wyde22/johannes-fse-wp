c238911e515f2a57bf8eac0bb5e8c1e0bce0352e
dewy <dewymercerais@gmail.com>
2025-05-22 14:33:49 +0200

    build: :lipstick: add image medium 1 pattern

---
+++ patterns/image-medium-1.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+   * Title: Image medium 1
+   * Slug: johannesfse-image-medium-1
+   * Description: Image medium 1 with left title, excerpt and buttons
+   * Categories: intro
+   * Keywords: image, intro
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Image medium 1"},"className":"image-medium-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-group image-medium-1"><!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"right":"var:preset|spacing|3xlarge","top":"var:preset|spacing|2xlarge","bottom":"var:preset|spacing|2xlarge"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-top:var(--wp--preset--spacing--2-xlarge);padding-right:var(--wp--preset--spacing--3-xlarge);padding-bottom:var(--wp--preset--spacing--2-xlarge)"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"p-default"} -->
+<p class="has-p-default-font-size" style="text-transform:uppercase">a place for the over-title</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-title {"level":1} /-->
+
+<!-- wp:post-excerpt {"style":{"spacing":{"padding":{"top":"var:preset|spacing|xlarge","bottom":"var:preset|spacing|xlarge"}}}} /-->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill","style":{"border":{"radius":"0px"},"typography":{"textTransform":"uppercase"}}} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline","style":{"border":{"radius":"0px"},"typography":{"textTransform":"uppercase"}}} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:post-featured-image {"aspectRatio":"1"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
