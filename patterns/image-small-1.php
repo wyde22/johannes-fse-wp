ddce39b1719b7c2ab1dbaa31200b5c823a08a327
dewy <dewymercerais@gmail.com>
2025-05-22 16:13:46 +0200

    build: :lipstick: add image small 1 pattern

---
+++ patterns/image-small-1.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+   * Title: Image small 1
+   * Slug: johannesfse-image-small-1
+   * Description: Image small with title and image of post
+   * Categories: intro
+   * Keywords: intro, post
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Image small 1"},"className":"image-small-1","style":{"spacing":{"padding":{"top":"var:preset|spacing|xlarge","bottom":"var:preset|spacing|xlarge"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group image-small-1" style="padding-top:var(--wp--preset--spacing--xlarge);padding-bottom:var(--wp--preset--spacing--xlarge)"><!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"right":"var:preset|spacing|3xlarge"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--3-xlarge)"><!-- wp:post-terms {"term":"category","textAlign":"center","className":"is-style-rounded-border","style":{"elements":{"link":{"color":{"text":"var:preset|color|johannes-grays-100"}}}},"textColor":"johannes-grays-100","fontSize":"p-default"} /-->
+
+<!-- wp:post-title {"level":1} /-->
+
+<!-- wp:post-date {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|large"}}}} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:post-featured-image {"aspectRatio":"1"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
