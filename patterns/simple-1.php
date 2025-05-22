d207105c8864e011c67f4c06372ad6d19dbdf74e
dewy <dewymercerais@gmail.com>
2025-05-22 15:27:22 +0200

    build: :lipstick: add simple 1 pattern

---
+++ patterns/simple-1.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Title: Simple 1
+ * Slug: johannesfse-simple-1
+ * Description: Banner simple 1
+ * Categories: banner
+ * Keywords: simple, banner, intro
+ * Viewport Width: 1200
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Simple 1"},"className":"simple-1","layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group simple-1"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|2xlarge","bottom":"var:preset|spacing|2xlarge"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--2-xlarge);padding-bottom:var(--wp--preset--spacing--2-xlarge)"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"p-default"} -->
+<p class="has-p-default-font-size" style="text-transform:uppercase">a place for the over-title</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-title /-->
+
+<!-- wp:post-excerpt {"style":{"spacing":{"padding":{"top":"var:preset|spacing|2xlarge","bottom":"var:preset|spacing|2xlarge"}}}} /-->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"0px"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline","style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"0px"}}} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
