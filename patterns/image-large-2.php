a9c9362628ffad2682e9667e8051ab0661c34faf
dewy <dewymercerais@gmail.com>
2025-05-22 15:03:34 +0200

    build: :lipstick: add image large 2 pattern

---
+++ patterns/image-large-2.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+   * Title: Image large 2
+   * Slug: johannesfse-image-large-2
+   * Description: Image large with title and buttons
+   * Categories: image
+   * Keywords: image, large
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Image large 2"},"className":"image-large-2","layout":{"type":"constrained"}} -->
+<div class="wp-block-group image-large-2"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"customOverlayColor":"#FFF","isUserOverlayColor":true,"isDark":false,"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|4xlarge","bottom":"var:preset|spacing|2xlarge"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignwide is-light" style="padding-top:var(--wp--preset--spacing--4-xlarge);padding-bottom:var(--wp--preset--spacing--2-xlarge)"><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#FFF"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"left","placeholder":"Rédigez le titre…","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"p-default"} -->
+<p class="has-text-align-left has-p-default-font-size" style="text-transform:uppercase">a place for the over title</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-title /-->
+
+<!-- wp:post-excerpt {"style":{"spacing":{"padding":{"top":"var:preset|spacing|xlarge","bottom":"var:preset|spacing|xlarge"}}}} /-->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"0px"}}} -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-outline","style":{"border":{"radius":"0px"},"typography":{"textTransform":"uppercase"}}} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase">button label</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
