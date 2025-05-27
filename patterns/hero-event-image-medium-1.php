82d760631e4fecc36ec39ee301ede7592fa3a272
dewy <dewymercerais@gmail.com>
2025-05-27 12:15:48 +0200

    build: :lipstick: add a new hero pattern

---
+++ patterns/hero-event-image-medium-1.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+   * Title: Hero event image medium 1
+   * Slug: johannesfse-hero-event-image-medium-1
+   * Description: Hero event image medium with title and image of post
+   * Categories: intro
+   * Keywords: intro, post
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Hero event image medium 1"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:post-terms {"term":"category","className":"is-style-rounded-border","style":{"typography":{"textTransform":"lowercase"},"elements":{"link":{"color":{"text":"var:preset|color|johannes-grays-100"}}}},"textColor":"johannes-grays-100"} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:post-title /-->
+
+<!-- wp:post-date /-->
+
+<!-- wp:post-featured-image {"aspectRatio":"16/9"} /-->
+
+<!-- wp:post-excerpt /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
