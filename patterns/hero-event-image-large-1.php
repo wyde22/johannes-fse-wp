26524e76312c66edc23de39d132c5af4c7cacd19
dewy <dewymercerais@gmail.com>
2025-05-28 14:07:12 +0200

    build: :lipstick: add new hero event pattern with css rules for responsive and custom style for a border in theme-settings.json

---
+++ patterns/hero-event-image-large-1.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+   * Title: Hero event image large 1
+   * Slug: johannesfse-hero-event-image-large-1
+   * Description: Hero event image large with title and image of post
+   * Categories: intro
+   * Keywords: intro, post
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Hero event image large 1"},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"customOverlayColor":"#FFF","isUserOverlayColor":false,"isDark":false,"align":"wide","className":"hero-event-image-large-1","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignwide is-light hero-event-image-large-1"><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#FFF"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Rédigez le titre…","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"p-small"} -->
+<p class="has-text-align-center has-p-small-font-size" style="text-transform:uppercase">event intro line</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-date {"textAlign":"center"} /-->
+
+<!-- wp:post-title {"textAlign":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|xlarge","bottom":"var:preset|spacing|xlarge"}}}} /-->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-fill","style":{"typography":{"textTransform":"uppercase"},"border":{"radius":"0px"}}} -->
+<div class="wp-block-button is-style-fill"><a class="wp-block-button__link wp-element-button" style="border-radius:0px;text-transform:uppercase"><strong>register</strong></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
