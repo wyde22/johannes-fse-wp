6ef3f1c5ffc33ed254a37ceb7c6ab147ed9e1b03
dewy <dewymercerais@gmail.com>
2025-05-28 14:12:30 +0200

    build: :lipstick: add press released pattern

---
+++ patterns/hero-list-press-released.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+   * Title: Hero list press released
+   * Slug: johannesfse-hero-list-press-released
+   * Description: Hero list with search button
+   * Categories: intro
+   * Keywords: intro, post
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Press released"},"className":"press-released","style":{"spacing":{"padding":{"top":"var:preset|spacing|base","bottom":"var:preset|spacing|base"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group press-released" style="padding-top:var(--wp--preset--spacing--base);padding-bottom:var(--wp--preset--spacing--base)"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"top","className":"is-style-minheight-column"} -->
+<div class="wp-block-column is-vertically-aligned-top is-style-minheight-column"><!-- wp:heading {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|3xlarge"}}}} -->
+<h2 class="wp-block-heading" style="padding-bottom:var(--wp--preset--spacing--3-xlarge)">Press released</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p><strong>Press Service</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"padding":{"bottom":"var:preset|spacing|xlarge"}}}} -->
+<p style="padding-bottom:var(--wp--preset--spacing--xlarge)">Fermentum dolor, netus vitae pretium mauris. Purus, fames vivamus urna urna, eu turpis commodo</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"verticalAlignment":"bottom"} -->
+<div class="wp-block-columns are-vertically-aligned-bottom"><!-- wp:column {"verticalAlignment":"bottom","className":"is-style-minheight-column"} -->
+<div class="wp-block-column is-vertically-aligned-bottom is-style-minheight-column"><!-- wp:search {"label":"Rechercher","showLabel":false,"width":75,"widthUnit":"%","buttonText":"Rechercher","style":{"elements":{"link":{"color":{"text":"var:preset|color|johannes-grays-0"}}},"border":{"radius":"4px"}},"backgroundColor":"johannes-grays-100","textColor":"johannes-grays-0"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:group {"style":{"border":{"width":"2px"},"spacing":{"padding":{"top":"var:preset|spacing|base","bottom":"var:preset|spacing|base","left":"var:preset|spacing|base","right":"var:preset|spacing|base"}}},"borderColor":"johannes-grays-50","layout":{"type":"flex","orientation":"vertical","flexWrap":"nowrap"}} -->
+<div class="wp-block-group has-border-color has-johannes-grays-50-border-color" style="border-width:2px;padding-top:var(--wp--preset--spacing--base);padding-right:var(--wp--preset--spacing--base);padding-bottom:var(--wp--preset--spacing--base);padding-left:var(--wp--preset--spacing--base)"><!-- wp:paragraph -->
+<p>Media relation</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Tel.: (+33) 1 23 45 67 89</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-outline","style":{"border":{"radius":"0px"},"typography":{"textTransform":"uppercase"}},"fontSize":"p-small"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-p-small-font-size has-custom-font-size wp-element-button" style="border-radius:0px;text-transform:uppercase">contact by email</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
