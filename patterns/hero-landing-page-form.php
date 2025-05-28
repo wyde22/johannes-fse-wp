8a62d59b8133f0ec6bd5bc7af5d107dd8dfa7aa7
dewy <dewymercerais@gmail.com>
2025-05-28 15:53:38 +0200

    build : :lipstick: add hero landing page form pattern with this css rule for responsive

---
+++ patterns/hero-landing-page-form.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+  * Title: Hero landing page form
+   * Slug: johannesfse-hero-landing-page-form
+   * Description: Hero landing page form with form contact
+   * Categories: contact
+   * Keywords: contact, form
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Hero landing page form"},"className":"hero-landing-page-form","layout":{"type":"constrained"}} -->
+<div class="wp-block-group hero-landing-page-form"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":50,"customOverlayColor":"#FFF","isUserOverlayColor":true,"isDark":false,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light"><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#FFF"></span><div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"right":"var:preset|spacing|2xlarge"}}}} -->
+<div class="wp-block-column is-vertically-aligned-center" style="padding-right:var(--wp--preset--spacing--2-xlarge)"><!-- wp:paragraph {"align":"left","placeholder":"Rédigez le titre…","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"p-small"} -->
+<p class="has-text-align-left has-p-small-font-size" style="text-transform:uppercase">a place for the over title</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-title /-->
+
+<!-- wp:post-excerpt {"style":{"spacing":{"padding":{"top":"var:preset|spacing|xlarge"}}}} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"var:preset|spacing|2xlarge","bottom":"var:preset|spacing|2xlarge","left":"var:preset|spacing|base","right":"var:preset|spacing|base"}},"border":{"radius":"4px"}},"backgroundColor":"johannes-grays-0"} -->
+<div class="wp-block-column is-vertically-aligned-center has-johannes-grays-0-background-color has-background" style="border-radius:4px;padding-top:var(--wp--preset--spacing--2-xlarge);padding-right:var(--wp--preset--spacing--base);padding-bottom:var(--wp--preset--spacing--2-xlarge);padding-left:var(--wp--preset--spacing--base)"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Contact forms</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
