2d53eddb5e9d55ac41973a1f6aeffd7a2a0bd11b
dewy <dewymercerais@gmail.com>
2025-06-10 14:22:07 +0200

    build: :lipstick: add pattern features list

---
+++ patterns/features-list.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+   * Title: Content features list
+   * Slug: johannesfse-content-features-list
+   * Description: Features list with left content, and image
+   * Categories: intro
+   * Keywords: intro, list
+   * Viewport Width: 1200
+   * Block Types:
+   * Post Types:
+   * Inserter: true
+   */
+
+?>
+
+<!-- wp:group {"metadata":{"name":"Features list"},"className":"features-list","layout":{"type":"constrained"}} -->
+<div class="wp-block-group features-list"><!-- wp:columns {"verticalAlignment":"center"} -->
+<div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}},"fontSize":"p-small"} -->
+<p class="has-p-small-font-size" style="text-transform:uppercase">label</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading -->
+<h2 class="wp-block-heading">This a headline for the  title part</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"padding":{"top":"var:preset|spacing|xlarge","bottom":"var:preset|spacing|xlarge"}}},"fontSize":"p-default"} -->
+<p class="has-p-default-font-size" style="padding-top:var(--wp--preset--spacing--xlarge);padding-bottom:var(--wp--preset--spacing--xlarge)">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque pellentesque pharetra quam enim porttitor gravida viverra. Tempus etiam aliquet sodales quisque consectetur pellentesque in tincidunt nam.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:paragraph -->
+<p><strong>Feature name</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"p-small"} -->
+<p class="has-p-small-font-size">Sit eu tellus pretium tristique dui faucibus ornare.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:paragraph -->
+<p><strong>Feature name</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"p-small"} -->
+<p class="has-p-small-font-size">Sit eu tellus pretium tristique dui faucibus ornare.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:paragraph -->
+<p><strong>Feature name</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"p-small"} -->
+<p class="has-p-small-font-size">Sit eu tellus pretium tristique dui faucibus ornare.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"aspectRatio":"3/4","scale":"cover"} -->
+<figure class="wp-block-image"><img alt="" style="aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
